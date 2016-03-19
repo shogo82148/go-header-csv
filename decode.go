dea3dbcb65cebd234f1ff4f4297b8a571af27f26
Ichinose Shogo <shogo82148@gmail.com>
2016-03-19 22:39:36 +0900

    support decoding a struct

---
+++ decode.go
@@ -0,0 +1,100 @@
+package headercsv
+
+import (
+	"encoding/csv"
+	"errors"
+	"io"
+	"reflect"
+	"strconv"
+)
+
+type Decoder struct {
+	header []string
+	r      *csv.Reader
+}
+
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{r: csv.NewReader(r)}
+}
+
+func (dec *Decoder) Decode(v interface{}) error {
+	if dec.header == nil {
+		record, err := dec.r.Read()
+		if err != nil {
+			return err
+		}
+		dec.SetHeader(record)
+	}
+
+	rv := reflect.ValueOf(v)
+	return dec.decodeRecord(rv)
+}
+
+func (dec *Decoder) SetHeader(header []string) error {
+	if dec.header != nil {
+		return errors.New("header has been alread set")
+	}
+	dec.header = header
+	return nil
+}
+
+func (dec *Decoder) decodeRecord(v reflect.Value) error {
+	record, err := dec.r.Read()
+	if err != nil {
+		return err
+	}
+
+	rt := recordType(v.Type())
+	switch rt := rt.(type) {
+	case namedRecordType:
+		for i, k := range dec.header {
+			v, _ := rt.FieldByName(v, k)
+			dec.decodeField(v, record[i])
+		}
+	default:
+		return errors.New("unsupported type")
+	}
+
+	return nil
+}
+
+func (dec *Decoder) decodeField(v reflect.Value, field string) error {
+	switch v.Kind() {
+	case reflect.Bool:
+		b, err := strconv.ParseBool(field)
+		if err != nil {
+			return err
+		}
+		v.SetBool(b)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		i, err := strconv.ParseInt(field, 10, 64)
+		if err != nil {
+			return err
+		}
+		if v.OverflowInt(i) {
+			return errors.New("overflow")
+		}
+		v.SetInt(i)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		i, err := strconv.ParseUint(field, 10, 64)
+		if err != nil {
+			return err
+		}
+		if v.OverflowUint(i) {
+			return errors.New("overflow")
+		}
+		v.SetUint(i)
+	case reflect.Float32, reflect.Float64:
+		n, err := strconv.ParseFloat(field, v.Type().Bits())
+		if err != nil {
+			return err
+		}
+		if v.OverflowFloat(n) {
+			return errors.New("overflow")
+		}
+		v.SetFloat(n)
+	case reflect.String:
+		v.SetString(field)
+	}
+	return nil
+}
---
+++ decode_test.go
@@ -0,0 +1,72 @@
+package headercsv
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+type AString struct {
+	A string
+}
+
+type ABool struct {
+	A bool
+}
+
+type AInt struct {
+	A int
+}
+
+type AFloat struct {
+	A float64
+}
+
+type ATag struct {
+	A string `csv:"a"`
+}
+
+func TestDecode(t *testing.T) {
+	testcases := []struct {
+		in  string
+		ptr interface{}
+		out interface{}
+	}{
+		// struct
+		{
+			"A\nb\n",
+			new(AString),
+			&AString{A: "b"},
+		},
+		{
+			"A\ntrue\n",
+			new(ABool),
+			&ABool{A: true},
+		},
+		{
+			"A\n123\n",
+			new(AInt),
+			&AInt{A: 123},
+		},
+		{
+			"A\n123\n",
+			new(AFloat),
+			&AFloat{A: 123},
+		},
+		{
+			"a\nb\n",
+			new(ATag),
+			&ATag{A: "b"},
+		},
+	}
+
+	for _, tc := range testcases {
+		d := NewDecoder(bytes.NewBufferString(tc.in))
+		if err := d.Decode(tc.ptr); err != nil {
+			t.Errorf("unexpected error: %v(%v)", err, tc)
+		}
+		if !reflect.DeepEqual(tc.ptr, tc.out) {
+			t.Errorf("%#v: got %v, want %v", tc.in, tc.ptr, tc.out)
+		}
+	}
+}
