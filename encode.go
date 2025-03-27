86a9e1cd29347d1fc4b54df79039ba800a825c52
ICHINOSE Shogo <shogo82148@gmail.com>
2025-03-27 14:12:03 +0900

    simplify isEmptyValue

--- encode.go
+++ encode.go
@@ -117,21 +117,17 @@ func (enc *Encoder) encodeRecord(v reflect.Value) error {
 	return enc.w.Write(record)
 }
 
-// steel from https://golang.org/src/encoding/json/encode.go
+// steel from https://github.com/golang/go/blob/1763ee199d33d2592332a29cfc3da7811718a4fd/src/encoding/json/encode.go#L318-L330
 func isEmptyValue(v reflect.Value) bool {
 	switch v.Kind() {
 	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
 		return v.Len() == 0
-	case reflect.Bool:
-		return !v.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return v.Float() == 0
-	case reflect.Interface, reflect.Ptr:
-		return v.IsNil()
+	case reflect.Bool,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32, reflect.Float64,
+		reflect.Interface, reflect.Pointer:
+		return v.IsZero()
 	}
 	return false
 }
